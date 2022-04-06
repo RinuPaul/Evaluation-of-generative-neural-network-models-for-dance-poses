09e3393b2d3090ca97eab95cee847f177fff1467
Rinu Elizabeth Paul <76184373+RinuPaul@users.noreply.github.com>
2022-04-06 10:06:07 +0200

    Add files via upload

---
+++ transflower/training/options/test_options.py
@@ -0,0 +1,20 @@
+from .base_options import BaseOptions
+
+
+class TestOptions(BaseOptions):
+    def __init__(self):
+        super(TestOptions, self).__init__()
+        parser = self.parser
+        parser.add_argument('--ntest', type=int, default=float("inf"), help='# of test examples.')
+        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
+        #  Dropout and Batchnorm has different behavioir during training and test.
+        parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
+
+        parser.set_defaults(model='test')
+        # To avoid cropping, the loadSize should be the same as fineSize
+        parser.set_defaults(load_size=parser.get_default('fine_size'))
+        self.is_train = False
+        self.parser = parser
+
+
