9ddfe0f9b1ee2b719588d9e1bc6a5b71804c5d95
Rinu Elizabeth Paul <76184373+RinuPaul@users.noreply.github.com>
2022-04-06 10:02:18 +0200

    Delete transflower directory

--- transflower/util/__init__.py
+++
@@ -1,4 +0,0 @@
-from models.util.array_util import *
-from models.util.norm_util import *
-from models.util.optim_util import *
-from models.util.shell_util import *
--- transflower/x_transformers/__init__.py
+++
@@ -1,2 +0,0 @@
-from x_transformers.x_transformers import XTransformer, Encoder, Decoder, CrossAttender, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
-from x_transformers.autoregressive_wrapper import AutoregressiveWrapper
